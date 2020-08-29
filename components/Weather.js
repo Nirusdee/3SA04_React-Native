6aeae261d659538a0a486f0119e71c2101b8f0a6
admin <admin@MacBook-Air-khxng-Piyapat.local>
2020-08-30 02:33:13 +0700

    section 5

--- components/Weather.js
+++ components/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, StyleSheet, ImageBackground } from 'react-native'
 import Forecast from './Forecast'
 
@@ -8,6 +8,26 @@ export default function Weather(props){
         description: '-',
         temp: 0
     }) 
+    
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+           fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=23ea0a2d303ee6fd0a0033408f7bdfd2`)
+                .then((response) => response.json())
+                .then((json) => {
+                     setForecastInfo({
+                       main: json.weather[0].main,
+                       description: json.weather[0].description,
+                       temp: json.main.temp
+               });
+          })
+           .catch((error) => {
+               console.warn(error);
+          });
+        }
+        }, [props.zipCode])
+    
+    
     
     return (
           
@@ -27,6 +47,7 @@ export default function Weather(props){
             Width: '100%',
             height: '100%'
     },
+    
 })
 
 
