39e866644348a7a80361bc7aff464d69bae44d6a
admin <admin@MacBook-Air-khxng-Piyapat.local>
2020-08-30 21:34:23 +0700

    section 7.2

--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -1,24 +1,29 @@
 import React from 'react'
-import { FlatList, View, Text, StyleSheet } from 'react-native'
+import { FlatList, View, Text, StyleSheet, Image } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { TouchableHighlight } from 'react-native-gesture-handler'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
 
 
 const availableZipItems = [
-    { place: 'Hatyai', code: '90110' },
-    { place: 'Trang', code: '92000' },
-    { place: 'Chiangmai', code: '50000' },
-    { place: 'Khonkaen', code: '40000' },
-    { place: 'Chonburi', code: '20000' },
+           { place: 'Hatyai', code: '90110', background : require('../hatyai.jpg') },
+           { place: 'Trang', code: '92000', background :require('../trang.jpg') },
+           { place: 'Chiangmai', code: '50000', background : require('../chiangmai.jpg')},
+           { place: 'Khonkaen', code: '40000', background : require('../khonkaen.jpg')},
+           { place: 'Chonburi', code: '20000', background : require('../chonburi.jpg')},
    ]
    
-   const ZipItem = ({place, code, navigation}) => (
+   const ZipItem = ({place, code, navigation, background}) => (
       <TouchableHighlight onPress={() => {
           navigation.navigate('Weather', {zipCode: code})
     }}>
-        <View style={styles.zipItem}>
-             <Text>{place}</Text>
-             <Text >{code}</Text>
+         <View style={styles.zipItem}>
+            <View style={styles.items}>
+                <Text style={styles.textSize}>{place}</Text>
+                <Text style={styles.textSize}>{code}</Text>
+            </View>
+            <Image source={background} style={styles.background}></Image>
         </View>
       </TouchableHighlight>
 )
@@ -26,26 +31,47 @@ const availableZipItems = [
    export default function ZipCodeScreen(){
     const navigation = useNavigation()
     return (
-          <FlatList
-             data={availableZipItems}
-             keyExtractor={item => item.code}
-             renderItem={({item}) => <ZipItem {...item} navigation={navigation}/>}
-          />
+        <SafeAreaView style={styles.container}>
+           <FlatList
+               data = {availableZipItems}
+               keyExtractor = {item => item.code}
+               renderItem = {({item}) => <ZipItem {...item} navigation={navigation}/>}
+           />
+        </SafeAreaView>
     )
    
 }
 
 const styles = StyleSheet.create({
-    zipItem: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between'
+        container: {
+           flex: 1,
+           marginTop: StatusBar.currentHeight || 0,
     },
-    zipPlace: {
-        flex: 1,
+        zipItem: {
+           flex: 1,
+           flexDirection: 'column',
+           backgroundColor: "rgb(220,220,220)",
+           padding: 20,
+           marginVertical: 8,
+           marginHorizontal: 16,
+           borderRadius: 10,
     },
-    zipCode: {
-        flex: 1,
+        items: {
+           flexDirection: 'row',
+           justifyContent: 'space-between',
+    },
+        zipPlace: {
+           flex: 1,
+    },
+        zipCode: {
+           flex: 1,
+    },
+        background: {
+           width: '100%',
+           height: 100,
+    },
+        textSize: {
+           fontSize: 22,
     }
 })
 
